03e699846442ed4cd60794147866c9d90aaf07b4
UEM <dassbhaskar2002@gmail.com>
2025-03-20 14:35:57 +0530

    Changes Done

--- Program.java
+++ Program.java
@@ -2,10 +2,9 @@ public class Program {
 public static void main(String args[]) {
 int a=10;
 int b=20;
-int temp=0;
-temp=a;
-a=b;
-b=temp;
+a=a+b;
+b=a-b;
+a=a-b;
 System.out.println(a);
 System.out.println(b);
 }
